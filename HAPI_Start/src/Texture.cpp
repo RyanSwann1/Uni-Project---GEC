4a45ccc03156d8baaf4eda6fb48b4ec6348f2e88
Ryan Swann <RyanSwann1992@gmail.com>
2019-07-22 00:36:43 +0100

    Texture now loads frames and checks for Alpha

--- HAPI_Start/src/Texture.cpp
+++ HAPI_Start/src/Texture.cpp
@@ -1,12 +1,19 @@
 #include "Texture.h"
 #include "Global.h"
 #include <assert.h>
+#include "Utilities/XMLParser.h"
 
+//Frame Details
+Frame::Frame(int y, int x, bool alpha)
+	: y(y),
+	x(x),
+	alpha(alpha)
+{}
+
+//Texture
 Texture::Texture()
 	: m_texture(nullptr),
-	m_width(0),
-	m_height(0),
-	m_alpha(false)
+	m_textureSize(0, 0)
 {}
 
 Texture::~Texture()
@@ -14,31 +21,38 @@ Texture::~Texture()
 	delete[] m_texture;
 }
 
+Rectangle Texture::getFrameRect(int tileID) const
+{
+	return Rectangle(tileID % m_columns * m_tileSize, m_tileSize, 
+		tileID / m_columns * m_tileSize, m_tileSize);
+}
+
 bool Texture::isAlpha() const
 {
 	return m_alpha;
 }
 
-bool Texture::load(const std::string & fileName)
+bool Texture::load(const std::string& xmlFileName, const std::string& textureFileName)
 {
 	assert(!m_texture);
-
-	bool textureLoaded = HAPI.LoadTexture(DATA_DIRECTORY + fileName, &m_texture, m_width, m_height);
+	XMLParser::parseTexture(m_tileSize, m_textureSize, m_columns, xmlFileName);
+	bool textureLoaded = HAPI.LoadTexture(DATA_DIRECTORY + textureFileName, &m_texture, m_textureSize.x, m_textureSize.y);
+	loadInFrames();
 	if (!textureLoaded)
 	{
 		return false;
 	}
 
-	const HAPISPACE::BYTE* currentTexturePixel = m_texture;
-	for (int i = 0; i < (m_width * m_height * BYTES_PER_PIXEL); i += BYTES_PER_PIXEL)
-	{
-		if (currentTexturePixel[3] < 255)
-		{
-			m_alpha = true;
-		}
-		
-		currentTexturePixel += BYTES_PER_PIXEL;
-	}
+	//const HAPISPACE::BYTE* currentTexturePixel = m_texture;
+	//for (int i = 0; i < (m_width * m_height * BYTES_PER_PIXEL); i += BYTES_PER_PIXEL)
+	//{
+	//	if (currentTexturePixel[3] < 255)
+	//	{
+	//		m_alpha = true;
+	//	}
+	//	
+	//	currentTexturePixel += BYTES_PER_PIXEL;
+	//}
 
 	return true;
 }
@@ -49,12 +63,38 @@ HAPISPACE::BYTE * Texture::getTexture()
 	return m_texture;
 }
 
-int Texture::getWidth() const
+Vector2i Texture::getSize() const
 {
-	return m_width;
+	return m_textureSize;
 }
 
-int Texture::getHeight() const
+void Texture::loadInFrames()
 {
-	return m_height;
+	int rows = m_textureSize.y / m_tileSize;
+	m_frames.reserve(static_cast<size_t>(rows * m_columns));
+	for (int frameID = 0; frameID < rows * m_columns; ++frameID)
+	{
+		Rectangle frameRect = getFrameRect(frameID);
+		bool frameHasAlpha = false;
+		int offset = frameID * m_tileSize;
+		HAPISPACE::BYTE* currentPixel = m_texture + offset;
+
+		for (int i = frameID * m_tileSize; i < frameRect.getRight() * frameRect.getBottom() * BYTES_PER_PIXEL; i += BYTES_PER_PIXEL)
+		{
+			*currentPixel = m_texture[i];
+			
+			if (currentPixel[3] < 255)
+			{
+				frameHasAlpha = true;
+			}
+		}
+
+		m_frames.emplace_back(frameRect.m_left, frameRect.m_top, frameHasAlpha);
+	}
+}
+
+bool Texture::isFrameAlpha(int ID) const
+{
+	assert(ID < m_frames.size());
+	return m_frames[ID].alpha;
 }
--- HAPI_Start/src/Texture.h
+++ HAPI_Start/src/Texture.h
@@ -2,26 +2,41 @@
 
 #include "HAPI_lib.h"
 #include <string>
+#include "Global.h"
+
+struct Frame
+{
+	Frame(int y, int x, bool alpha = false);
+
+	int y;
+	int x;
+	bool alpha;
+};
 
 class Texture
 {
 public:
 	Texture();
 	~Texture();
-	Texture(const Texture&) = delete;
-	Texture& operator=(const Texture&) = delete;
-	Texture(Texture&&) = delete;
-	Texture&& operator=(Texture&&) = delete;
+	//Texture(const Texture&) = delete;
+	//Texture& operator=(const Texture&) = delete;
+	//Texture(Texture&&) = delete;
+	//Texture&& operator=(Texture&&) = delete;
 
+	Rectangle getFrameRect(int tileID) const;
 	bool isAlpha() const;
-	bool load(const std::string& fileName);
+	bool load(const std::string& xmlFileName, const std::string& textureFileName);
 	HAPISPACE::BYTE* getTexture();
-	int getWidth() const;
-	int getHeight() const;
+	Vector2i getSize() const;
 
 private:
 	HAPISPACE::BYTE* m_texture;
-	int m_width;
-	int m_height;
+	std::vector<Frame> m_frames;
+	Vector2i m_textureSize;
+	int m_columns;
+	int m_tileSize;
 	bool m_alpha;
+
+	void loadInFrames();
+	bool isFrameAlpha(int ID) const;
 };
