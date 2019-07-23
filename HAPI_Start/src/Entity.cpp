fa46b92cc2391c299f6ba57961725f74a09888e6
Ryan Swann <RyanSwann1992@gmail.com>
2019-07-23 23:04:21 +0100

    Entity assigned starting position

--- HAPI_Start/src/Entity.cpp
+++ HAPI_Start/src/Entity.cpp
@@ -1,6 +1,8 @@
 #include "Entity.h"
 #include "Window.h"
+#include <assert.h>
 
+//Turret
 Turret::Turret(Texture & tileSheet)
 	: m_position(),
 	m_base(tileSheet),
@@ -19,4 +21,38 @@ void Turret::setPosition(Vector2i position)
 	m_position = position;
 	m_base.setPosition(position);
 	m_head.setPosition(position);
+}
+
+//Entity
+Entity::Entity(Texture & tileSheet, int tileID, const std::vector<Vector2i>& entityPath)
+	: m_entityPath(entityPath),
+	m_sprite(tileSheet),
+	m_active(false)
+{
+	m_sprite.setID(tileID);
+	m_position = m_entityPath.back();
+	m_sprite.setPosition(m_entityPath.back());
+	m_entityPath.pop_back();
+}
+
+void Entity::activate()
+{
+	assert(!m_active);
+	m_active = true;
+}
+
+void Entity::update()
+{
+	if (m_active)
+	{
+
+	}
+}
+
+void Entity::render(const Window & window)
+{
+	if (m_active)
+	{
+
+	}
 }
--- HAPI_Start/src/Entity.h
+++ HAPI_Start/src/Entity.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Sprite.h"
+#include <vector>
 
 enum class TurretType
 {
@@ -13,7 +14,8 @@ enum class EntityID
 	TURRET_CANNON_BASE = 180,
 	TURRET_CANNON_HEAD = 249,
 	TURRET_MISSLE_BASE = 181,
-	TURRET_MISSLE_HEAD = 204
+	TURRET_MISSLE_HEAD = 204,
+	SOILDER_GREEN = 245
 };
 
 class Window;
@@ -29,4 +31,20 @@ struct Turret
 	Vector2i m_position;
 	Sprite m_base;
 	Sprite m_head;
+};
+
+class Entity
+{
+public:
+	Entity(Texture& tileSheet, int tileID, const std::vector<Vector2i>& entityPath);
+
+	void activate();
+	void update();
+	void render(const Window& window);
+
+private:
+	std::vector<Vector2i> m_entityPath;
+	Vector2i m_position;
+	Sprite m_sprite;
+	bool m_active;
 };
